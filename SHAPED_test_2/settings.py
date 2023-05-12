626f436cbdc06cf5d126ffb427129ca0a06bc5ad
Marcelo Livramento <molivramento@gmail.com>
2023-05-12 12:59:08 -0300

    config app patients: admin, models, apps

--- SHAPED_test_2/settings.py
+++ SHAPED_test_2/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_filters',
 
     # Internal apps
+    'app.patients'
 ]
 
 MIDDLEWARE = [
--- app/patients/models.py
+++ app/patients/models.py
@@ -1,3 +1,20 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=128)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_age(self):
+        return int((datetime.date.today() - self.birth_date).days / 365)
+
+    class Meta:
+        verbose_name = 'Patient'
+        verbose_name_plural = 'Patients'
+        ordering = ['name']
