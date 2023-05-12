18a99013001b1a26537abee4ab4b102bcbdef768
Marcelo Livramento <molivramento@gmail.com>
2023-05-12 17:19:57 -0300

    finish exams

--- app/exams/models.py
+++ app/exams/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    professional = models.CharField(max_length=128)
+    patient = models.ForeignKey('patients.Patient', on_delete=models.SET_NULL, null=True)
+    date = models.DateField()
+    weight = models.FloatField()
+    height = models.FloatField()
+
+    def __str__(self):
+        return f'{self.professional} - {self.patient} - {self.date}'
+
+    class Meta:
+        verbose_name = 'Exam'
+        verbose_name_plural = 'Exams'
+        ordering = ['-date']
